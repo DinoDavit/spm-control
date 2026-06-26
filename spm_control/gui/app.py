319c9cf9a982e42a16973b0d106c16fe6d7f8080
Davit Moreno <davitmoreno@Davits-MacBook-Pro.local>
2026-06-26 00:19:19 -0500

    Created main app architecture and also some assets to go along for raster scanner app

--- spm_control/gui/app.py
+++ spm_control/gui/app.py
@@ -5,7 +5,9 @@ ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("blue")
 
 layout = {
-    "save_file": (0, 0, 0.06, 0.1),
+    # Creating a main layout of dimensions for main panels
+    # These main panels should remain roughly the same despite mode
+    "settings": (0, 0, 0.06, 0.1),
     "modes": (0, 0.1, 0.06, 0.8),
     "mode_options": (0.06, 0, 0.744, 0.1),
     "mode_display": (0.06, 0.1, 0.744, 0.8),
@@ -19,40 +21,31 @@ class Application(ctk.CTk):
         super().__init__()
         self.title("SPM App")
 
+        # Getting max dimensions of monitor
         max_width = self.winfo_screenwidth()
         max_height = self.winfo_screenheight()
+
         self.geometry(f"{max_width}x{max_height}")
 
+        self.panels = {}
+        # Getting name from layout and creating corresponding frames
         for name, (x, y, width, height) in layout.items():
-            self.frame = ctk.CTkFrame(
+
+            self.panels[name] = ctk.CTkFrame(
                 self,
                 border_width = 2,
                 border_color = "white",
                 fg_color = "black",
             )
-        
-            self.frame.place(
+
+            # Defining placement and dimensions of frames
+            self.panels[name].place(
                 relx = x,
                 rely = y,
                 relwidth = width,
                 relheight = height,
             )
-
-        # Building the 3x3 frames that will be used
-
         
-        # self.top_left = ctk.CTkFrame(self, border_width=2)
-        # self.top_center = ctk.CTkFrame(self, border_width=2)
-        # self.top_right = ctk.CTkFrame(self, border_width=2)
-
-        # self.left = ctk.CTkFrame(self, border_width=2)
-        # self.center = ctk.CTkFrame(self, border_width=2)
-        # self.right = ctk.CTkFrame(self, border_width=2)
-
-        # self.bottom_left = ctk.CTkFrame(self, border_width=2)
-        # self.bottom_center = ctk.CTkFrame(self, border_width=2)
-        # self.bottom_right = ctk.CTkFrame(self, border_width=2)
-
 
 def main():
     app = Application()
