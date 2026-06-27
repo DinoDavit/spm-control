a625624105dad72730241a21caade7c7fa790bb6
Davit Moreno <davitmoreno@Davits-MacBook-Pro.local>
2026-06-27 14:04:37 -0500

    Fixed name bug

--- spm_control/gui/modes/page_helpers.py
+++ spm_control/gui/modes/page_helpers.py
@@ -3,21 +3,30 @@ import ttkbootstrap as ttk
 import customtkinter as ctk
 import yaml
 
-def update_config(vars, file_name):
-    with open(file_name) as f:
-        doc = yaml.load(f)
+def getDimensions(parent):
+    return parent.winfo_screenwidth(), parent.winfo_screenheight()
 
-    for name, val in vars.items():
-        doc[name] = val
+def addFrame(parent, name, dimensions):
+    parent.name = ctk.CTkFrame()
+    parent.name.place(
+    relx = dimensions[x],
+    rely = dimensions[y],
+    relwidth = dimensions[width],
+    relheight = dimensions[height])
 
-    with open('file_to_edit.yaml', 'w') as f:
-        yaml.dump(doc, f)
+def update_config(updates, file_name):
+    with open(file_name, "r") as f:
+        doc = yaml.safe_load(f) or {}
+
+    for name, value in updates.items():
+        doc[name] = value
+
+    with open(file_name, "w") as f:
+        yaml.safe_dump(doc, f, sort_keys=False)
     
 
 def load_required_panels(page, panels, required_panels):
     page.panels = panels
-
-    print(panels)
     
     for name in required_panels:
         setattr(page, name, panels[name])
@@ -25,29 +34,33 @@ def load_required_panels(page, panels, required_panels):
 def fillOptions(parent, options, function_calls):
     print("HELLO WORLD")
     
+def addButtonDisplay(parent, name, func):
+    print("HELLO WROLD")
+
 def addButton(parent, name, func):
-    parent.button = ctk.CTkButton(
+    parent.name = ctk.CTkButton(
         master = parent,
         text = name,
         command = func
     )
 
-    parent.button.pack(padx=20, pady=20)
+    parent.name.pack(padx=20, pady=20)
 
 def addCheckbox(parent, name):
     print("HELL OWORLD")
 
 
 def addRangeInput(parent, name):
-        parent.entry = ctk.CTkEntry( # might need to do parent.entry[name] have to check since entry might have multiple...
+        parent.name = ctk.CTkEntry( # might need to do parent.entry[name] have to check since entry might have multiple...
             master=parent, 
             placeholder_text="",
             width=30,
         )
 
-        parent.entry.pack(pady=10)
-        val = parent.entry.get()
+        parent.name.pack(pady=10)
+        val = parent.name.get()
         parent.vars[name] = val
+        print(parent.vars.items())
 
 def addSingleEntry(parent, name):
     row = ctk.CTkFrame(parent)
